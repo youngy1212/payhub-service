cfb27cd3f1aac37e3ca5e6674a8b58830d9c3a6e
youngy1212 <117425085+youngy1212@users.noreply.github.com>
2024-10-11 21:17:25 +0900

    Initial commit

---
+++ src/main/java/com/payhub/PayhubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.payhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayhubServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayhubServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payhub/PayhubServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayhubServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
